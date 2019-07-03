962385e6354d7c90eec4a65f0ba3a37637a2e917
Niki Stavrou <nikist95@gmail.com>
2019-07-03 11:32:31 +0200

    avg arr dly plot

---
+++ R/capacity/plot-avg-arr-dly.R
@@ -0,0 +1,46 @@
+apt_arrival_dly <- function(airport, since, to){
+  
+  all_details_apt <- all_details %>%
+    filter(apt_name == airport) %>%
+    filter(yyyy >= since & yyyy <= to) %>%
+    group_by(yyyy, delay_group) %>%
+    summarise(delay = sum(delay)) 
+  
+  all_summaries_apt <- all_summaries %>%
+    filter(apt_name == airport) %>%
+    filter(yyyy >= since & yyyy <= to) %>%
+    group_by(yyyy) %>%
+    summarise(arrival_delay = sum(arrival_delay), num_arrivals = sum(num_arrivals))
+  
+  all_apt <- inner_join(all_details_apt, all_summaries_apt, by = "yyyy")
+  
+  all_apt <- all_apt %>%
+    mutate(avg = arrival_delay/num_arrivals,
+           dly = delay/num_arrivals) %>%
+    select(- delay)
+  
+  all_apt$yyyy <- as.character(all_apt$yyyy)
+  
+  g <- ggplot(all_apt, aes(x = yyyy, y = dly, fill = delay_group))
+  
+  apt_arr_dly_plot <- g + 
+    geom_bar(stat = "identity") +
+    theme_pru() +
+    scale_fill_manual(values = c("#fed976", "#41ab5d", "#cb181d", "#bdbdbd"),
+                      #values = pru_pal()(9)[c(2,4,7,9)],
+                      name = "",
+                      breaks = c("ATC attributed", "W", "G", "Other"),
+                      labels = c(
+                         "ATC attributed [C, S, I, T]",
+                         "Weather [W, D]",
+                         "Airport Capacity [G]",
+                         "Other [all other codes]")) +
+    labs(x = "",
+         y = "Avg. airport ATFM arrival delay per arrival (min)",
+         title = paste("Average airport arrival ATFM delay -", airport, "airport"),
+         subtitle = "") +
+    #ylim(0,1) +
+    theme(legend.position = "bottom")
+  
+  apt_arr_dly_plot
+}
