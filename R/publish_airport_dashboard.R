4c86bc49468d8146c39e5a076cc39fa521e2c708
Enrico Spinielli <enrico.spinielli@eurocontrol.int>
2021-06-17 17:16:15 +0200

    draft June 2021 release

---
+++ R/publish_airport_dashboard.R
@@ -0,0 +1,40 @@
+library(fs)
+library(here)
+library(magrittr)
+library(dplyr)
+library(purrr)
+library(blogdown)
+
+# this could fail if MS Windows keeps hold of some files!
+# Then repeat it
+dest <- here("content","dashboard","stakeholder", "airport","db")
+src <- here("..","pru-airport-dashboard")
+
+dir_ls(
+  path = dest,
+  glob = "*.Rmd",
+  invert = TRUE,
+  recurse = TRUE) %>%
+  walk(.f = file_delete)
+  
+
+# copy files ...
+dir_ls(
+  path = here(src,"docs"),
+  glob = "*.md",
+  type = "file",
+  invert = TRUE) %>%
+  walk(.f = ~ file_copy(.x, dest))
+
+# ... and the JS libs
+dir_copy(path = here(src,"docs", "libs"),
+         new_path = here(dest, "libs"),
+         overwrite = TRUE)
+
+file_copy(here(src, "data", "APT_DSHBD_AIRPORT.csv"),
+          here("data-config"),
+          overwrite = TRUE)
+
+# regenerate _index.html
+# this does NOT work
+# blogdown::build_site(run_hugo = FALSE, build_rmd = c(here(dest, "_index.Rmd")))
