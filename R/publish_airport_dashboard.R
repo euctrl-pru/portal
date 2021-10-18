3213dee974a8bec9ec801d5b7738256f08e203d7
Enrico Spinielli <enrico.spinielli@eurocontrol.int>
2021-10-18 18:28:56 +0200

    updated airport dashboard

--- R/publish_airport_dashboard.R
+++ R/publish_airport_dashboard.R
@@ -38,4 +38,7 @@ file_copy(here(src, "data", "APT_DSHBD_AIRPORT.csv"),
 
 # regenerate _index.html
 # this does NOT work
-blogdown::build_site(build_rmd = c(here(dest, "_index.Rmd")))
+# blogdown::build_site(build_rmd = c(here(dest, "_index.Rmd")))
+
+# maybe this
+rmarkdown::render_site(here(dest, "_index.Rmd"),  encoding = 'UTF-8')
