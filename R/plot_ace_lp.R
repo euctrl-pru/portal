9aeaf367017b56832d24d0b0792d41ab1a761c76
OscarAlfaro73 <oscar.alfaro@eurocontrol.int>
2022-04-26 17:28:22 +0200

    addec chart and dynamic figures

---
+++ R/plot_ace_lp.R
@@ -0,0 +1,107 @@
+## libraries
+# library(dplyr)
+# library(stringr)
+library(plotly)
+
+
+# ACE_landing_page <- read.csv(
+#   file  = here::here ("content","economics","ACE_landing_page_data.csv")) 
+# 
+# if(ncol(ACE_landing_page)==1){
+#   ACE_landing_page <- read.csv2(
+#     file  = here::here ("content","economics","ACE_landing_page_data.csv")) 
+# }
+# 
+# ACE_landing_page <- ACE_landing_page%>%
+#   as_tibble%>%
+#   mutate_all(as.numeric)%>%
+#   rename(year_data=1)
+# 
+# costs_per_cph <- ACE_landing_page %>% filter(year_data == max(year_data)) %>% select(costs_per_cph) %>% unlist %>% unname
+
+plot_ACE <- ACE_landing_page %>%
+  plot_ly(
+    width = 500, height = 350,
+    x = ~ year_data,
+    y = ~ costs_per_cph,
+    yaxis = "y1",
+    marker = list(color =('##4F81BD')),
+    text = ~ paste("  <b>",round(costs_per_cph,0),"</b>"),
+    textangle = -90,
+    textposition = "inside",
+    insidetextanchor =  "start",
+    textfont = list(color = '#FFFFFF', size = 14),
+    type = "bar",
+    hoverinfo = "none",
+    showlegend = F
+  )%>%
+  add_trace(
+    inherit = FALSE,
+    x = ~ year_data,
+    y = ~ costs_per_cph+20,
+    yaxis = "y1",
+    # colors = c('#4F81BD'),
+    mode = 'text',
+    text = ~ paste("<b>",if_else(costs_per_cph_change_perc >0, "+", ""),round(costs_per_cph_change_perc*100,if_else(costs_per_cph_change_perc >=1, 0, 1)), "%","</b>",sep = ""),
+    textfont = list(color = 'black', size = 14),
+    type = 'scatter',  mode = 'lines',
+    hoverinfo = "none",
+    showlegend = F
+    
+  )%>%
+  add_trace(
+    x = ~ year_data,
+    y = ~ round(index_costs,1), 
+    inherit = FALSE,
+    yaxis = "y2",
+    type = 'scatter',  mode = 'lines', name = 'ATM/CNS provision costs',
+    line = list(color = "#1F497D"),
+    hovertemplate = paste('<b>ATM/CNS costs index</b>: <br>%{y}',
+                          "<extra></extra>",
+                          sep = "")
+  )%>%
+  add_trace(
+    x = ~ year_data,
+    y = ~ round(index_cph,1), 
+    inherit = FALSE,
+    yaxis = "y2",
+    type = 'scatter',  mode = 'lines', name = 'Composite flight-hours',
+    line = list(color = "#E46C0A"),
+    hovertemplate = paste('<b>Composite flight-hours index</b>: <br>%{y}',
+                          "<extra></extra>",
+                          sep = "")
+  )%>%
+  layout(autosize = F,
+    xaxis = list(
+      title = "",
+      # automargin = T,
+      # tickvals = 2014:2019,
+      autotick = F,
+      # ticktext = paste0(ACE_landing_page$year_data,"n"),
+      showgrid = F
+    ),
+    
+    yaxis = list(
+      title = paste("\U20AC","per composite flight-hour"),
+      titlefont   = list(size = 13),
+      # tickformat=",.0%", ticks = 'outside',
+      zeroline = T, showline = F, showgrid = T
+    ),
+    yaxis2 = list (
+      overlaying = "y",
+      side = "right",
+      title = paste ("Index of costs and traffic", "<br>","(", min(ACE_landing_page$year_data), " = 100)",sep = ""),
+      titlefont = list(size = 13),
+      range = list(40, 10+round(max(ACE_landing_page$index_costs, ACE_landing_page$index_cph)/10)*10),
+      showgrid = F,
+      automargin = T
+    ),
+    bargap = 0.4,
+    legend = list(orientation = 'h', xanchor = "left", x = -0.05, y = -0.05),
+    # hovermode = "x unified",
+    uniformtext=list(minsize=10, mode='show')
+  ) %>%
+  config(displaylogo = FALSE, modeBarButtons = list(list("toImage"))
+  )
+
+plot_ACE
